ed1bbbd5c20846b6b55fe4c3e0e3ed6f3993c070
Andrey Anisimov <maddbuzz@gmail.com>
2023-03-30 20:50:07 +0800

    refactor

--- test/controllers/web/repositories_controller_test.rb
+++ test/controllers/web/repositories_controller_test.rb
@@ -29,24 +29,27 @@ module Web
       github_repo_id = 1
       github_repo = github_repos.find { |repo| repo['id'] == github_repo_id }
 
+      assert { !Repository.exists?(github_repo_id:) }
+      repository_last_id = Repository.last.id
+
       stub_request(:get, "#{GITHUB_API_PATH}repositories/#{github_repo_id}")
         .to_return(body: github_repo.to_json, status: 200, headers: { content_type: 'application/json' })
 
       post repositories_url, params: { repository: { github_repo_id: } }
       assert_redirected_to repositories_url
 
+      last_repository = Repository.last
+      assert { last_repository.github_repo_id == github_repo_id }
+      assert { last_repository.id > repository_last_id }
+      assert { last_repository.user == current_user }
+
       github_repo.deep_symbolize_keys!
-      assert do
-        current_user.repositories.exists?(
-          github_repo_id: github_repo[:id],
-          link: github_repo[:html_url],
-          owner_name: github_repo[:owner][:login], # need deep_symbolize_keys!, not just symbolize_keys!
-          repo_name: github_repo[:name],
-          language: github_repo[:language],
-          repo_created_at: github_repo[:created_at],
-          repo_updated_at: github_repo[:updated_at]
-        )
-      end
+      assert { last_repository.link == github_repo[:html_url] }
+      assert { last_repository.owner_name == github_repo[:owner][:login] } # need deep_symbolize_keys!, not just symbolize_keys!
+      assert { last_repository.repo_name == github_repo[:name] }
+      assert { last_repository.language == github_repo[:language] }
+      assert { last_repository.repo_created_at == github_repo[:created_at] }
+      assert { last_repository.repo_updated_at == github_repo[:updated_at] }
     end
 
     test 'should show repository' do
