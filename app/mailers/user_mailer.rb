4a679f72f9c6d821678f7742fb7ef99aba827228
Andrey Anisimov <maddbuzz@gmail.com>
2023-04-11 00:07:21 +0800

    refactor user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,14 +4,16 @@ class UserMailer < ApplicationMailer
   default from: "#{Rails.application.class.module_parent_name}@checks.com"
 
   def repo_check_failed
-    @check = params[:check]
-    repo = @check.repository
-    @repo_full_name = "#{repo.owner_name}/#{repo.repo_name}"
-    @user = repo.user
-    mail(to: @user.email, subject: t('.subject', repo_full_name: @repo_full_name))
+    repo_check_or_verification_failed
   end
 
   def repo_check_verification_failed
+    repo_check_or_verification_failed
+  end
+
+  private
+
+  def repo_check_or_verification_failed
     @check = params[:check]
     repo = @check.repository
     @repo_full_name = "#{repo.owner_name}/#{repo.repo_name}"
