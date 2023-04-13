2b374e96a8de550c61db004d7bc676167d8045ac
Andrey Anisimov <maddbuzz@gmail.com>
2023-04-13 15:35:58 +0800

    remove last migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_125503) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_062607) do
   create_table "repositories", force: :cascade do |t|
     t.integer "github_id"
     t.string "link"
